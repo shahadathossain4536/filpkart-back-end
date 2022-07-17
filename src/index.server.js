a57f4fc76c16618d14b6831adb6e05ddb3cfe757
shahadat4536 <shahadathossain4536@gmail.com>
2022-07-17 09:34:03 +0600

    signup

--- src/index.server.js
+++ src/index.server.js
@@ -6,7 +6,9 @@ const mongoose = require("mongoose");
 
 env.config();
 
-app.use(bodyParser());
+//routes
+const userRoutes = require("./routes/user");
+
 // mongodb+srv://admin:<password>@cluster0.yam67.mongodb.net/?retryWrites=true&w=majority
 // serverApi: ServerApiVersion.v1,
 mongoose
@@ -20,17 +22,8 @@ mongoose
   .then(() => {
     console.log("database connected");
   });
-app.get("/", (req, res, next) => {
-  res.status(200).json({
-    message: "Hello from Server",
-  });
-});
-
-app.post("/data", (req, res, next) => {
-  res.status(200).json({
-    message: req.body,
-  });
-});
+app.use(bodyParser());
+app.use("/api", userRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
